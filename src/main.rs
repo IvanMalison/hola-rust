515a5b5aa1f11ea04f086b2d0392af691001ecec
Ivan Malison <IvanMalison@gmail.com>
2019-06-23 15:33:51 -0700

    Use Rc to properly allow access

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use core::hash::Hash;
 use std::collections::HashMap;
+use std::rc::Rc;
 
 struct Cacher<A, B, T>
     where T: Fn(A) -> B
 {
-    cache: HashMap<A, B>,
+    cache: HashMap<A, Rc<B>>,
     calculation: T,
 }
 
@@ -21,10 +22,10 @@ where A: Eq + Hash + Clone,
     }
 
     fn add(&mut self, arg: A) {
-        self.cache.insert(arg.clone(), (self.calculation)(arg.clone()));
+        self.cache.insert(arg.clone(), Rc::new((self.calculation)(arg.clone())));
     }
 
-    fn run(&mut self, arg: A) -> B {
+    fn run(&mut self, arg: A) -> Rc<B> {
         match self.cache.get(&arg) {
             Some(v) => v.clone(),
             None => {
@@ -32,30 +33,6 @@ where A: Eq + Hash + Clone,
                 self.run(arg)
             },
         }
-    }
-
-    fn run_ref_alt(&mut self, arg: A) -> &B {
-        match self.cache.get(&arg) {
-            // I think one can't just use _v below because it changes the
-            // lifetime of the immutable self borrow that results from to extend
-            // long enough to interfere with the mutable borrow of self that is
-            // required by the call to add. It's not entirely clear to me why
-            // the compiler isn't smart enough to see that we don't actually
-            // need the extended lifetime for the cases where we actually do
-            // mutation, but I suspect that this is the issue
-            Some(_v) => self.cache.get(&arg).unwrap(), // _v,
-            None => {
-                self.add(arg.clone());
-                self.run_ref_alt(arg)
-            },
-        }
-    }
-
-    fn run_ref(&mut self, arg: A) -> &B {
-        if ! self.cache.contains_key(&arg) {
-            self.add(arg.clone());
-        }
-        self.cache.get(&arg).unwrap()
     }
 }
 
@@ -74,12 +51,14 @@ fn main() {
     let v3 = c.run(1);
     let v4 = c.run(2);
     let v5 = c.run(3);
+    // c.cache.clear();
 
     println!("{}", v3);
     println!("{}", v4);
     println!("{}", v5);
     println!("{}", v5);
-    println!("{}", c.run_ref(4));
-    println!("{}", c.run_ref_alt(4));
+    // println!("{}", v1);
+    println!("{}", c.run(4));
+    println!("{}", c.run(4));
     println!("{}", c.run(4));
 }
